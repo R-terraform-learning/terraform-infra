5c8655ec4053195b907844f96c8ccb9c1a4d01a1
revathyshunmugham <srevathy22@gmail.com>
2026-05-30 11:09:20 +0530

    arn updated

--- accounts/dev/backend.tf
+++ accounts/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "revathy-s3-bucket-12345"
-    key            = "eks/dev/terraform.tfstate"
+    bucket         = "revathy-s3-bucket-for-tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "R-dev-tf-lock-state"
   }
