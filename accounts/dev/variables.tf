ad29dd19003ec876f84cddc2edfcb24d7c4ba23c
revathyshunmugham <srevathy22@gmail.com>
2026-05-30 11:11:38 +0530

    arn updated

--- accounts/dev/variables.tf
+++ accounts/dev/variables.tf
@@ -1,15 +1,14 @@
 variable "bucket_name" {
-  type = string
+  type        = string
   description = "name of the bucket to be created"
 }
 
 variable "env" {
-  type = string
+  type        = string
   description = "environment name"
 }
 
 variable "file_name" {
-  type = string
+  type        = string
   description = "file name that is referred from s3 module"
 }
-
